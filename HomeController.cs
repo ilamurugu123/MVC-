5e26c61fe8c0aff9d710624dd8fb2abee1cb2a08
ilamurugu123 <51310835+ilamurugu123@users.noreply.github.com>
2020-01-10 09:40:30 +0530

    MVC
    MVC files

---
+++ HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestPartialView.Models;
+
+namespace TestPartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TextBoxBinding objTextboxBindings = new TextBoxBinding();
+            return View(objTextboxBindings); // Returning model
+        }
+
+        [ChildActionOnly]
+        public ActionResult _TestIndex(TextBoxBinding viewModel)
+        {
+            return PartialView("_TestIndex", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult _TestIndex(TextBoxBinding viewModel, string garbage)
+        {
+
+            viewModel.ValueHolders.Add(new ValueHolder { ID = viewModel.ID, ValueProperty = viewModel.Name });
+            return PartialView("_TestIndex", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult GetData(string[] names)
+        {
+
+
+
+            return View();
+        }
+    }
+}
---
+++ TextBoxBinding.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestPartialView.Models
+{
+    public class TextBoxBinding
+    {
+        public TextBoxBinding()
+        {
+            ValueHolders = new List<ValueHolder>();
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<ValueHolder> ValueHolders { get; set; } // List of TextboxValues
+    }
+
+    public class ValueHolder
+    {
+        public int ID { get; set; }
+        public string ValueProperty { get; set; }
+    }
+
+}
